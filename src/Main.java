d12f3d8feed179c462c1f016b2976a65e5997a79
sivavinod2004 <sivapriyaav2028@cs.sjcetpalai.ac.in>
2025-07-22 11:12:12 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
-
-    // TODO: Create a method to display student details
+class Student{
+    String name;
+    int rollNumber;
+    char grade;
+    public Student(String name,int rollNumber,char grade){
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+    public void displayDetails(){
+        System.out.println("Name:\t"+name);
+        System.out.println("Rollnumber:\t"+rollNumber);
+        System.out.println("grade:\t"+grade);
+    }
+}
 public class Main {
-      public static void main(String[] args) {
-        // TODO: Create a Student object and call the method to display details
+    public static void main(String[] args) {
+    Student student1=new Student("siva",66,'S');
+        student1.displayDetails();
     }
 }
